a3e9c19d358f4c82b324476c6f23c1fbc726526b
xinggevip <1511844263@qq.com>
2020-03-21 21:21:57 +0800

    lambda + stream 写法

--- src/main/java/com/qiangssvip/service/impl/ICategoryImpl.java
+++ src/main/java/com/qiangssvip/service/impl/ICategoryImpl.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -22,15 +22,30 @@ public class ICategoryImpl implements ICategory {
 
     @Override
     public ResponseVo<List<CategoryVo>> selectAll() {
-        ArrayList<CategoryVo> arrayList = new ArrayList<>();
+//        ArrayList<CategoryVo> arrayList = new ArrayList<>();
         List<Category> categories = categoryMapper.selectAll();
-        for (Category category : categories) {
-            if (category.getParentId().equals(0)) {
-                CategoryVo categoryVo = new CategoryVo();
-                BeanUtils.copyProperties(category,categoryVo);
-                arrayList.add(categoryVo);
-            }
-        }
-        return ResponseVo.successs(arrayList);
+//        for (Category category : categories) {
+//            if (category.getParentId().equals(0)) {
+//                CategoryVo categoryVo = new CategoryVo();
+//                BeanUtils.copyProperties(category,categoryVo);
+//                arrayList.add(categoryVo);
+//            }
+//        }
+        // lambda + stream
+        List<CategoryVo> collect = categories.stream()
+                .filter(e -> e.getParentId().equals(0))  // 返回符合条件的数据
+                .map(this::category2CategoryVo)         // 遍历数据
+//                .map(e -> category2CategoryVo(e))     // 和上一句效果一致，idea推荐使用上面的写法
+                .collect(Collectors.toList());
+
+        return ResponseVo.successs(collect);
     }
+
+    private CategoryVo category2CategoryVo(Category category) {
+        CategoryVo categoryVo = new CategoryVo();
+        BeanUtils.copyProperties(category,categoryVo);
+        return categoryVo;
+    }
+
+
 }
