4198fd89bb4d6c990102f1c2e26d15f22ef296e6
xinggevip <1511844263@qq.com>
2020-03-21 10:53:19 +0800

    自定义异常状态码

--- src/main/java/com/qiangssvip/exception/RuntimeExceptionHandler.java
+++ src/main/java/com/qiangssvip/exception/RuntimeExceptionHandler.java
@@ -2,15 +2,18 @@ package com.qiangssvip.exception;
 
 import com.qiangssvip.enums.ResponseEnum;
 import com.qiangssvip.service.vo.ResponseVo;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class RuntimeExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)  // 要处理的异常类型
     @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // 自定义异常状态码
     public ResponseVo handle(RuntimeException e){
         return ResponseVo.error(ResponseEnum.ERROR,e.getMessage());
     }
